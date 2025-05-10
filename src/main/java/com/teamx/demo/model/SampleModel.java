ee9d75c9417c45f280dfa1ff6c5ea4daac786aaf
swetha4444 <swethasaseendran4@gmail.com>
2025-05-10 15:03:06 -0400

    first commit

---
+++ src/main/java/com/teamx/demo/model/SampleModel.java
@@ -0,0 +1,5 @@
+package com.teamx.demo.model;
+
+// Add fields and methods as needed
+public class SampleModel {
+}
---
+++ src/main/java/com/teamx/demo/service/SampleService.java
@@ -0,0 +1,13 @@
+package com.teamx.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getSampleData() {
+        return "Sample Data";
+    }
+
+    // Additional business logic methods can be added here
+}
