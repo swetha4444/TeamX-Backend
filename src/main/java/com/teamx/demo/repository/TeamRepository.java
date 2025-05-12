b5bd4c2be55fd4c9e9d46e49f2ef0bedbe793083
swetha4444 <swethasaseendran4@gmail.com>
2025-05-12 02:44:33 -0400

    Add TeamRepository interface for team data access

---
+++ src/main/java/com/teamx/demo/repository/TeamRepository.java
@@ -0,0 +1,12 @@
+package com.teamx.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.teamx.demo.model.Team;
+
+public interface TeamRepository extends MongoRepository<Team, String> {
+    List<Team> findByContestId(String contestId);
+    List<Team> findByUserEmail(String userEmail);
+}
