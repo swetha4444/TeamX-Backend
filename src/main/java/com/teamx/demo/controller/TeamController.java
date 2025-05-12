0a9a081e9d51c609ab859a476b30c6e20fec24dd
swetha4444 <swethasaseendran4@gmail.com>
2025-05-12 12:35:11 -0400

    Add endpoints to retrieve team captain and vice captain by team ID

--- src/main/java/com/teamx/demo/controller/TeamController.java
+++ src/main/java/com/teamx/demo/controller/TeamController.java
@@ -1,16 +1,12 @@
 package com.teamx.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.teamx.demo.model.Team;
 import com.teamx.demo.service.TeamService;
@@ -38,5 +34,23 @@ public class TeamController {
     public ResponseEntity<?> getTeamsByUserEmail(@PathVariable String userEmail) {
         List<Team> teams = teamService.getTeamsByUserEmail(userEmail);
         return ResponseEntity.ok(teams);
+    }
+    
+    @GetMapping("/{teamId}/captain")
+    public ResponseEntity<?> getTeamCaptain(@PathVariable String teamId) {
+        Optional<Team> teamOpt = teamService.getTeamById(teamId);
+        if(teamOpt.isPresent()){
+            return ResponseEntity.ok(teamOpt.get().getCaptain());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Team not found");
+    }
+    
+    @GetMapping("/{teamId}/viceCaptain")
+    public ResponseEntity<?> getTeamViceCaptain(@PathVariable String teamId) {
+        Optional<Team> teamOpt = teamService.getTeamById(teamId);
+        if(teamOpt.isPresent()){
+            return ResponseEntity.ok(teamOpt.get().getViceCaptain());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Team not found");
     }
 }
